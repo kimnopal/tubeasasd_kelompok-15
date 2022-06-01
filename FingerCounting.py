8157d4cfce07799ecb86764fafd9a76419c2e51d
Shafnaa <saujanashafi2003@gmail.com>
2022-06-01 11:41:07 +0700

    menambahkan foto serta mengimport foto kedalam program

--- FingerCounting.py
+++ FingerCounting.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+import os
+import HandTrackingModule as htm
 
 # definisi lebar dan tinggi kamera
 wCam, hCam = 640, 480
@@ -11,10 +13,22 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
+# mengambil semua nama foto
+folderPath = "images"
+myList = os.listdir(folderPath)
+
+# mengambil semua path foto
+overlayList = []
+for imPath in myList:
+    # mengimport foto
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    overlayList.append(image)
+print(len(overlayList))
+
 while True:
     # membuat/membaca frame kamera
     success, img = cap.read()
-
+    print(len(overlayList))
     # merender img/video dan menajalankan kamera
     cv2.imshow("Image", img)
     # untuk memberikan delay 1ms
